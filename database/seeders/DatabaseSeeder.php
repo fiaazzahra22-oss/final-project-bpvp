2ff432662509eaed28b96c477d1ec056c2ea353b
fia azzahra <fiaazzahra22@gmail.com>
2026-04-20 15:27:45 +0700

    day13 templating

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'fia',
+            'email' => 'fia@example.com',
+            'password' => '12345678'
         ]);
     }
 }
